907b0e08271d05f62fe9dc0d21ca9e0579a361e3
Nathan Wu <nathanwu23@gmail.com>
2020-03-21 18:45:22 +1100

    plot limited to nyquist frequency

--- plot.py
+++ plot.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
+import sys
 
-with open("readData.txt") as file:
+file_name = sys.argv[1]
+
+with open(file_name) as file:
     readData = file.read()
 lines = readData.splitlines()
-
-# with open("inputData.txt") as file:
-#     readData = file.read()
-# lines = readData.splitlines()
-
 
 data = []
 index = 0
@@ -21,22 +19,8 @@ for i in range(numLines//2):
         data.append(float(lines[i]) * 2)
     index += 1
 
-# numLines = len(lines)
-# # for i in range(4000):
-# for i in range(numLines):
-#     if index % 10 == 0:
-#         # double amplitude of each entry because of nyquist frequency
-#         data.append(float(lines[i]))
-#     index += 1
 
-# xVals = [i for i in range(0, numLines, 10)]
 xVals = [i for i in range(0, numLines//2, 10)]
-# plt.xscale('log', basex=, subsx = [1,2,3,4,5,6,7,8,9])
 plt.axis([0, 20000, 0, 1000])
 
-# plt.yscale('log', basey=10)
-# fig, ax = plt.subplots()
-# plt.set_xscale('log', basex=10)
-# plt.set_yscale('log', basey=10)
-plt.plot(xVals, data)
 plt.show()
